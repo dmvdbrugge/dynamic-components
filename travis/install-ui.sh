6c5b96b9213f5b6b333dc2942b6e58175f3a74ea
Dave van der Brugge <dmvdbrugge@gmail.com>
2018-06-25 23:23:31 +0200

    Prettify install script, and return exit code

--- travis/install-ui.sh
+++ travis/install-ui.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+
+# Clone ui and setup dependency dirs
 git clone --branch=release https://github.com/krakjoe/ui
-cd ui
-mkdir -p deps/{lib,include}
+cd ui && mkdir -p deps/{lib,include}
+
+# Clone and build libui
 git clone --depth=1 https://github.com/andlabs/libui
-cd libui
-mkdir build
-cd build
+cd libui && mkdir build && cd build
 cmake ..
 make -j4
-cp out/libui.* ../../deps/lib
-cp ../ui*.h ../../deps/include
+
+# Copy built libui things to ui-dependency dirs
+cp out/libui.* ../../deps/lib && cp ../ui*.h ../../deps/include
+
+# Prepare and build ui
 cd ../..
-phpize
-./configure --with-ui=deps
-make
-cd ..
-echo "extension=ui.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+phpize && ./configure --with-ui=deps
+make && echo "extension=ui.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
+# Return if building ui worked
+exit $?
