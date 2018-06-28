40b7d4a54fa422b3f2b71ed4a5a657edfcfefc22
Dave van der Brugge <dmvdbrugge@gmail.com>
2018-06-28 23:10:38 +0200

    Try with tgz from pecl

--- travis/install-ui.sh
+++ travis/install-ui.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-cp travis/ui.* `php-config --extension-dir`
-echo "extension=ui.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+# Get ui from pecl and setup dependency dirs
+wget https://pecl.php.net/get/UI-2.0.0.tgz
+tar -xzf UI-2.0.0.tgz
+cd UI-2.0.0 && mkdir -p deps/{lib,include}
+
+# Clone and build libui
+git clone --depth=1 https://github.com/andlabs/libui
+cd libui && mkdir build && cd build
+cmake ..
+make -j4
+
+# Copy built libui things to ui-dependency dirs
+cp out/libui.* ../../deps/lib && cp ../ui*.h ../../deps/include
+
+# Prepare and build ui
+cd ../..
+phpize && ./configure --with-ui=deps
+make && make install && echo "extension=ui.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
+# Return if building ui worked
+exit $?
