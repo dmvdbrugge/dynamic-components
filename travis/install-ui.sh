dfa605c1d03f6861cd02098d542b7d6e3d01dd01
Dave van der Brugge <dmvdbrugge@gmail.com>
2018-10-01 22:32:11 +0200

    Work plz

--- travis/install-ui.sh
+++ travis/install-ui.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir extensions
+EXT_DIR="`pwd`/extensions"
+
 # Get ui and setup dependency dirs
 wget https://pecl.php.net/get/UI-2.0.0.tgz -q
 tar -xzf UI-2.0.0.tgz
@@ -18,7 +21,10 @@ cp out/libui.* ../../deps/lib && cp ../ui*.h ../../deps/include
 # Prepare and build ui
 cd ../..
 phpize && ./configure --with-ui=deps
-make && make install && echo "extension=ui.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+make && make EXTENSION_DIR="${EXT_DIR}" install
+
+if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then echo "extension=${EXT_DIR}/ui.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini; fi
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then echo "extension=${EXT_DIR}/ui.so" >> /usr/local/etc/php/7.1/php.ini; fi
 
 # Return if building ui worked
 exit $?
