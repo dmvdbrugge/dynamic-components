7fa0dc81759d5bebd704e356892ccd26343fda7d
Dave van der Brugge <dmvdbrugge@users.noreply.github.com>
2018-10-01 23:52:34 +0200

    Travis again (#6)
    * Back to the release version of php-ui
    * Use the release version of php-ui
    * Use libui commit mentioned in readme of UI at point of release
    * We need a GUI in Travis: https://docs.travis-ci.com/user/gui-and-headless-browsers/#using-xvfb-to-run-tests-that-require-a-gui
    * Remove code style as thats in PrettyCI now. Also comment out builds that break anyway.
    * Travis OSX (#7)
      * Don't clone, just use the direct zip
      * Try add osx
      * Listen to brew, add to path. Also try cache brew
      * Cleanup plus allow linux to fail for now
    
    Closes #2 😁

--- travis/install-ui.sh
+++ travis/install-ui.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-# Clone ui and setup dependency dirs
-git clone --branch=release https://github.com/krakjoe/ui
-cd ui && mkdir -p deps/{lib,include}
+# Need to define different EXTENSION_DIR for OSX
+mkdir extensions
+EXT_DIR="`pwd`/extensions"
 
-# Clone and build libui
-git clone --depth=1 https://github.com/andlabs/libui
+# Target ini differs per os too
+if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then INI_TARGET=~/.phpenv/versions/$(phpenv version-name)/etc/php.ini; fi
+if [[ "${TRAVIS_OS_NAME}" == "osx"   ]]; then INI_TARGET=/usr/local/etc/php/7.1/php.ini; fi
+
+# Get ui and setup dependency dirs
+wget https://pecl.php.net/get/UI-2.0.0.tgz -q
+tar -xzf UI-2.0.0.tgz
+cd UI-2.0.0 && mkdir -p deps/{lib,include}
+
+# Get and build libui
+wget https://github.com/andlabs/libui/archive/ce37d12d230cea529bf6f5ac1d3bc76b9a75bbbd.zip -qO libui.zip
+unzip -q libui.zip && mv libui-* libui
 cd libui && mkdir build && cd build
 cmake ..
 make -j4
@@ -16,7 +26,7 @@ cp out/libui.* ../../deps/lib && cp ../ui*.h ../../deps/include
 # Prepare and build ui
 cd ../..
 phpize && ./configure --with-ui=deps
-make && echo "extension=ui.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+make && make EXTENSION_DIR="${EXT_DIR}" install && echo "extension=${EXT_DIR}/ui.so" >> ${INI_TARGET}
 
 # Return if building ui worked
 exit $?
